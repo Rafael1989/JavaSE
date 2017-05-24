39f6b52267f95ce2700c55d00be745522fa1582e
Rafael <Rafael@10.177.15.248>
2017-05-24 14:05:54 -0300

    teste

--- javase/src/br/com/java/array/StringArgsMetodoMain.java
+++ javase/src/br/com/java/array/StringArgsMetodoMain.java
@@ -4,6 +4,7 @@ public class StringArgsMetodoMain {
 	public static void main(String[] args) {
 		for(int i = 0;i<args.length;i++){
 			System.out.println(args[i]);
+			System.out.println();
 		}
 	}
 }
